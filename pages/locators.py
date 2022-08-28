bc07f153abfab9a7236f0c434154602d7b896901
Aleksandr Sushchenko <sushchenko.aa@phystech.edu>
2022-08-28 20:37:01 +0300

    test 'add product to basket'

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "button[type=\"submit\"].btn-add-to-basket")
+    NAME_IN_CATALOG = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_IN_CATALOG = (By.CSS_SELECTOR, "div.col-sm-6.product_main p:nth-child(2)")
+    NAME_IN_ALERT = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon:nth-child(1) .alertinner strong")
+    PRICE_IN_ALERT = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon:nth-child(3) .alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,39 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        price = self.should_be_text_with_price_and_get_it()
+        name = self.should_be_text_with_name_and_get_it()
+        self.press_button(*ProductPageLocators.BASKET_BUTTON)
+        self.solve_quiz_and_get_code()
+        self.should_be_same_name_in_alert(name)
+        self.should_be_same_price_in_alert(price)
+
+    def press_button(self, how, what):
+        self.should_be_button(how, what)
+        self.browser.find_element(how, what).click()
+
+    def should_be_text_with_price_and_get_it(self):
+        price = self.get_element_text(*ProductPageLocators.PRICE_IN_CATALOG)
+        assert price is not None, "Can not find price of product"
+        return price
+
+    def should_be_text_with_name_and_get_it(self):
+        name = self.get_element_text(*ProductPageLocators.NAME_IN_CATALOG)
+        assert name is not None, "Can not find name of product"
+        return name
+
+    def should_be_button(self, how, what):
+        assert self.is_element_present(how, what), f"Can not find button with request {what}"
+
+    def should_be_same_price_in_alert(self, price):
+        new_price = self.get_element_text(*ProductPageLocators.PRICE_IN_ALERT)
+        assert new_price is not None, "Can not find price in alert"
+        assert new_price == price
+    
+    def should_be_same_name_in_alert(self, name):
+        new_name = self.get_element_text(*ProductPageLocators.NAME_IN_ALERT)
+        assert new_name is not None, "Can not find name in alert"
+        assert new_name == name
